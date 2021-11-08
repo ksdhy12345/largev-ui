531337f1d4c4ed23aa8442784bedc3bfe11a8c6e
于波 <1932817371@qq.com>
2021-11-08 16:37:04 +0800

    edit

--- src/main.js
+++ src/main.js
@@ -12,13 +12,13 @@ import Option from './components/Option'
 
 import './assets/iconfont.css'
 
-const app = createApp(Test);
+const app = createApp(App);
 app.component(Button.name, Button)
 app.component(Input.name, Input)
 app.component(Dialog.name, Dialog)
 app.component(Form.name, Form)
 app.component(FormItem.name, FormItem)
-app.component(Select.name,Select),
-app.component(Option.name,Option),
+app.component(Select.name, Select),
+app.component(Option.name, Option),
 
-app.mount('#app')
+  app.mount('#app')
